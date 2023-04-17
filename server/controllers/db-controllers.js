211c4c816d2efcb4ae0d4df77c4b4a39db90dbdc
Muneeb T <muneeb.t@neutrinos.co>
2023-04-17 11:41:49 +0530

    Database storage displayed
    API created for fetching storage used in database and it displayed in user interface.

--- server/controllers/db-controllers.js
+++ server/controllers/db-controllers.js
@@ -2,13 +2,10 @@ import mongoose, { mongo } from 'mongoose';
 
 const getStorageUsed = async (req, res) => {
   try {
-    const dbStats = await mongoose.connection.db.command({
-      dbStats: 1,
-      scale: 1024 * 1024,
-    });
-    console.log(dbStats);
+    const dbStats = await mongoose.connection.db.stats({ scale: 1024 * 1024 });
+    const { storageSize } = dbStats;
     const storage = {
-      usedStorage: (dbStats.dataSize / (1024 * 1024)).toFixed(2),
+      usedStorage: storageSize.toFixed(2),
       totalStorage: (512).toFixed(2),
     };
     res.status(200).json({
--- server/routes/database.js
+++ server/routes/database.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dbControllers from '../controllers/db-controllers.js';
+import verifyJwtToken from '../middlewares/verify-jwt-token.js';
+
 const router = express.Router();
 
 router.get('/storage', dbControllers.getStorageUsed);
