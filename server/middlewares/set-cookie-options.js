8939e4e3ea7cece8140af8b82da2587fcfe65d95
Muneeb T <muneeb.t@neutrinos.co>
2023-04-19 12:51:07 +0530

    Bug fix

--- server/middlewares/set-cookie-options.js
+++ server/middlewares/set-cookie-options.js
@@ -1,7 +1,7 @@
 const setCookieOptions = (req, res, next) => {
   res.cookie = (name, value, options) => {
     const cookieOptions = options || {};
-    cookieOptions.sameSite = none;
+    cookieOptions.sameSite = 'none';
     cookieOptions.secure = true;
     res.cookie(name, value, cookieOptions);
   };
