ef5149a0d32d3fc054b72194e695550b80c5e5f3
Muneeb T <muneeb.t@neutrinos.co>
2023-04-19 14:47:53 +0530

    Set cookie options middleware changed

--- server/middlewares/set-cookie-options.js
+++ server/middlewares/set-cookie-options.js
@@ -1,6 +1,11 @@
 const setCookieOptions = (req, res, next) => {
-  res.header('Set-Cookie', `SameSite=None; Secure`);
-  next();
+  const originalCookie = res.cookie;
+  res.cookie = function (name, value, options) {
+    const newOptions = Object.assign({}, options, {
+      sameSite: 'none',
+      secure: true,
+    });
+    originalCookie.call(this, name, value, newOptions);
+  };
 };
-
 export default setCookieOptions;
