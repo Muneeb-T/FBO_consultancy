06f518f42134df408c383b79567f78d704caf26e
Muneeb T <muneeb.t@neutrinos.co>
2023-04-24 15:06:18 +0530

    Role based authorization added

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -3,12 +3,14 @@ import employeeControllers from '../controllers/employee-controllers.js';
 import validate from '../middlewares/validate-request.js';
 import schemas from '../utils/request-schemas.js';
 import verifyJwtToken from '../middlewares/verify-jwt-token.js';
+import authorize from '../middlewares/authorize.js';
 
 const router = express.Router();
 
 router.post(
   '/register',
   verifyJwtToken,
+  authorize('admin'),
   validate(schemas.register, 'body'),
   employeeControllers.register,
 );
